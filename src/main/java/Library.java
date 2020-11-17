f7e573f63bd42960192b926dcd74e4c088708f09
stmcf <stevenmcfarlane@hotmail.co.uk>
2020-11-17 20:34:19 +0000

    MVP completed.

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,2 +1,27 @@
+import java.util.ArrayList;
+
 public class Library {
+    private ArrayList<Book> stock;
+    private int capacity;
+
+    public Library(int capacity){
+        this.stock = new ArrayList<>();
+        this.capacity = capacity;
+    }
+
+
+    public int stockCount(){
+        return this.stock.size();
+    }
+
+    public void addBook(Book book){
+        if (libraryCanAcceptBook()) {
+            this.stock.add(book);
+        }
+    }
+
+    public boolean libraryCanAcceptBook(){
+        return this.stockCount() < this.capacity;
+    }
+
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,2 +1,43 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class LibraryTest {
+
+    private Library library;
+    private Book book;
+    private Book book2;
+    private Book book3;
+
+    @Before
+    public void setUp(){
+        library = new Library(20);
+        book = new Book("Breakfast of Champions", "Kurt Vonnegut", "Satire");
+        book2 = new Book("The Twits", "Roald Dahl", "Childrens");
+        book3 = new Book("Finding Ultra", "Rich Roll", "Sports");
+    }
+
+    @Test
+    public void canCountStock(){
+        assertEquals(0, library.stockCount());
+    }
+
+    @Test
+    public void canAddBookToStock(){
+        library.addBook(book);
+        library.addBook(book2);
+        library.addBook(book3);
+        assertEquals(3, library.stockCount());
+    }
+
+    @Test
+    public void libraryIsAtCapacitySoCannotAddBook(){
+        library = new Library(1);
+        library.addBook(book);
+        library.addBook(book2);
+        library.addBook(book3);
+        assertEquals(1, library.stockCount());
+    }
+
 }
