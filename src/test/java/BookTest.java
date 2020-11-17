8296347cbcbe28bc0111b9e362169ad302227024
stmcf <stevenmcfarlane@hotmail.co.uk>
2020-11-17 17:14:10 +0000

    book class added and tested for author, genre, title

--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -1,2 +1,29 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BookTest {
+
+    private Book book;
+
+    @Before
+    public void setUp(){
+        book = new Book("Breakfast of Champions", "Kurt Vonnegut", "Satire");
+    }
+    @Test
+    public void bookHasTitle(){
+        assertEquals("Breakfast of Champions", book.getTitle());
+    }
+
+    @Test
+    public void bookHasAuthor(){
+        assertEquals("Kurt Vonnegut", book.getAuthor());
+    }
+    @Test
+    public void  bookHasGenre(){
+        assertEquals("Satire", book.getGenre());
+    }
+
+
 }
